72bebe7499e8cbd26ace3bccad6472d9e0823bfa
maxogden <max@maxogden.com>
2021-08-10 15:29:31 -0700

    add changes from server

--- get_animal_photos.py
+++ get_animal_photos.py
@@ -21,42 +21,43 @@ args = parser.parse_args()
 BASE_PATH = args.base_path
 
 def copy_files(detector_output_dir, images_output):
-	for dirpath, dirnames, filenames in os.walk(detector_output_dir):
-		if dirpath is not detector_output_dir:
-			continue
+  for dirpath, dirnames, filenames in os.walk(detector_output_dir):
+    if dirpath is not detector_output_dir:
+      continue
 
-		for i in filenames:
-			detector_output = os.path.join(detector_output_dir, i)
-			if datetime.now() - datetime.fromtimestamp(os.stat(detector_output).st_ctime) < timedelta(0, 0, 0, 0, 0, 1):
-				files = []
+    for i in filenames:
+      detector_output = os.path.join(detector_output_dir, i)
+      if datetime.now() - datetime.fromtimestamp(os.stat(detector_output).st_ctime) < timedelta(0, 0, 0, 0, 0, 1):
+        files = []
 
-				with open (detector_output) as f:
-					images = json.load(f)
-					
-				for entry in images['images']:
-					print(entry['file'])
+        with open (detector_output) as f:
+          images = json.load(f)
+          
+        for entry in images['images']:
+          imgPath = i.split('.')[0] + '/' + entry['file']
+          print(entry['file'])
 
-					# add first and last image from folder
-					if entry['file'] == images['images'][0]['file'] or entry['file'] == images['images'][-1]['file']:
-						print("first/last")
-						files.append(entry['file'])
-						continue
+          # add first and last image from folder
+          if entry['file'] == images['images'][0]['file'] or entry['file'] == images['images'][-1]['file']:
+            print("first/last")
+            files.append(imgPath)
+            continue
 
-					for detection in entry['detections']:
+          for detection in entry['detections']:
 
-						if detection['category'] == '1' and entry['max_detection_conf'] > 0.8:
-							files.append(entry['file'])
-							continue
-				
-				
-				for image_file in files:
-					source = os.path.join(BASE_PATH, image_file)
-					destination = os.path.join(images_output, image_file)
+            if detection['category'] == '1' and entry['max_detection_conf'] > 0.8:
+              files.append(imgPath)
+              continue
+        
+        
+        for image_file in files:
+          source = os.path.join(BASE_PATH, image_file)
+          destination = os.path.join(images_output, image_file)
 
-					if not os.path.isdir(os.path.dirname(os.path.join(images_output, image_file))):
-						os.makedirs(os.path.dirname(os.path.join(images_output, image_file)))
-			
-					shutil.copy(source, destination)
+          if not os.path.isdir(os.path.dirname(os.path.join(images_output, image_file))):
+            os.makedirs(os.path.dirname(os.path.join(images_output, image_file)))
+      
+          shutil.copy(source, destination)
 
 if __name__ == "__main__":
-	copy_files(args.file_location,args.output_dest)
+  copy_files(args.file_location,args.output_dest)

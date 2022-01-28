68e2e86d3695fe8b49c9cd883edf03ac7aafd5af
Maxwell Ogden <max@maxogden.com>
2022-01-27 18:34:19 -0800

    use rclone copy not sync

--- multicity/sync-photos.sh
+++ multicity/sync-photos.sh
@@ -1,5 +1,5 @@
-echo "1. Downloading UWIN_Test_Dataset..."
-time rclone sync -P gdrive:"UWIN_Test_Dataset" /home/compbio/GDrive/UWIN_Test_Dataset
+echo "1. Syncing down UWIN_Test_Dataset..."
+time rclone copy -P gdrive:"UWIN_Test_Dataset" /home/compbio/GDrive/UWIN_Test_Dataset
 
-echo "2. Uploading UWIN_Test_Dataset..."
-time rclone sync -P /home/compbio/GDrive/UWIN_Test_Dataset gdrive:"UWIN_Test_Dataset"
+echo "2. Syncing up UWIN_Test_Dataset..."
+time rclone copy -P /home/compbio/GDrive/UWIN_Test_Dataset gdrive:"UWIN_Test_Dataset"
--- sync-photos.sh
+++ sync-photos.sh
@@ -1,11 +1,11 @@
 # sync old photos
-#time rclone sync -P gdrive:"Wildlife Camera Team/UWIN Protocols & Datasheets/Photos" /home/compbio/Photos
+#time rclone copy -P gdrive:"Wildlife Camera Team/UWIN Protocols & Datasheets/Photos" /home/compbio/Photos
 
 echo "1. Syncing Photo_Upload..."
-time rclone sync -P gdrive:"Photo_Upload" /home/compbio/GDrive/Photo_Upload
+time rclone copy -P gdrive:"Photo_Upload" /home/compbio/GDrive/Photo_Upload
 
 echo "2. Syncing Photo_MegaOutput..."
-time rclone sync -P gdrive:"Photo_MegaOutput" /home/compbio/GDrive/Photo_MegaOutput
+time rclone copy -P gdrive:"Photo_MegaOutput" /home/compbio/GDrive/Photo_MegaOutput
 
 echo "3. Uploading Photo_MegaOutput..."
-time rclone sync -P /home/compbio/GDrive/Photo_MegaOutput gdrive:"Photo_MegaOutput"
+time rclone copy -P /home/compbio/GDrive/Photo_MegaOutput gdrive:"Photo_MegaOutput"

ed71621d572f5e55c134c2f2bbbda3e03aebd39f
Baez2222 <49249262+Baez2222@users.noreply.github.com>
2022-02-01 00:56:16 -0600

    collects exif data

--- convert-md-to-csv.py
+++ convert-md-to-csv.py
@@ -1,14 +1,26 @@
 import csv
 import os
 import json
+import argparse
+from PIL import Image, ExifTags
 
-BASE_PATH = "/home/compbio/GDrive/UWIN_Test_Dataset/Photo_Upload/first_upload"
 
-def createCSV():
+parser = argparse.ArgumentParser(description = 'Convert MegaDetector JSON file to csv.\
+                                                Returns a csv of the same name as the json file.')
+parser.add_argument('md_output', type=str,
+                    help='Path to MegaDetectors JSON file')
+parser.add_argument('base_path', type=str,
+                    help='Path to photos in MegaDetectors JSON file')
+args = parser.parse_args()
+
+BASE_PATH = args.base_path
+
+def createCSV(md_output):
+  md_output_csv = md_output.split(".")[0] + ".csv"
   counter = 0
-  with open("first_upload.csv", "w+") as c:
+  with open(md_output_csv, "w+") as c:
     writer = csv.writer(c)
-    with open("first_upload.json", "r") as detector_output:
+    with open(md_output, "r") as detector_output:
         images = json.load(detector_output)
 
         for entry in images['images']:
@@ -16,10 +28,6 @@ def createCSV():
           # print(counter)
           photoDir = os.path.join(BASE_PATH, entry['file'])
           # print(photoDir)
-
-          is_animal = False
-          is_human = False
-          is_car = False
 
           numAnimalDetections = 0
           numHumanDetections = 0
@@ -50,13 +58,38 @@ def createCSV():
               # 	if detection["conf"] > maxDetectionConf:
               # 		maxDetectionConf = detection["conf"]
               # print(detection)
+              
+              img = Image.open(os.path.join(BASE_PATH, entry['file']))
+            exif = {
+                ExifTags.TAGS[k]: v
+                    for k, v in img._getexif().items()
+                    if k in ExifTags.TAGS
+            }
 
-            writer.writerow([photoDir, str(numAnimalDetections), str(jsonAnimalDetection), str(numHumanDetections), str(jsonOtherDetection), maxDetectionConf, maxDetectionConfHuman])
+            # not sure if every camera has these values in their metadata
+            # DateTime, ImageDescription, Make, Model, ShutterSpeedValue, ApertureValue, ISOSpeedRatings
+            try: exifTimestamp = exif["DateTime"]
+            except: exifTimestamp = None
+            try: exifImageDescription = exif["ImageDescription"]
+            except: exifImageDescription = None
+            try: exifMake = exif["Make"]
+            except: exifMake = None
+            try: exifModel = exif["Model"]
+            except: exifModel = None
+            try: exifShutterSpeedValue = exif["ShutterSpeedValue"]
+            except: exifShutterSpeedValue = None
+            try: exifApertureValue = exif["ApertureValue"]
+            except: exifApertureValue = None
+            try: exifISOSpeedRatings = exif["ISOSpeedRatings"]
+            except: exifISOSpeedRatings = None
+
+            writer.writerow([photoDir, str(numAnimalDetections), str(jsonAnimalDetection), str(numHumanDetections), str(jsonOtherDetection), maxDetectionConf, 
+                             maxDetectionConfHuman, exifTimestamp, exifImageDescription, exifMake, exifModel, exifShutterSpeedValue, exifApertureValue, exifISOSpeedRatings])
           except KeyError:
             print(KeyError)
             continue
         detector_output.close()
     c.close()
   
-
-createCSV()
+if __name__ == "__main__":
+    createCSV(args.md_output)
